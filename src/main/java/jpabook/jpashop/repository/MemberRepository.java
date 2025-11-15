bdb03dfa1ff95ea8ca0c9c6e1e97a5d5a3b541da
Son-Gyeongi <thsruddl77@gmail.com>
2025-11-15 17:33:10 +0900

    section5 17. 회원 서비스 개발

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -3,15 +3,28 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext // JPA 사용, JPA가 제공하는 표준 어노테이션
+/*
+    @PersistenceContext // JPA 사용, JPA 가 제공하는 표준 어노테이션
     private EntityManager em;
+*/
+    // @Autowired - spring data JPA 가 Autowired injection 될 수 있게 도와준다.
+    public final EntityManager em;
+
+/*
+    public MemberRepository(EntityManager em) {
+        this.em = em;
+    }
+*/
 
     public void save(Member member) {
         em.persist(member);
